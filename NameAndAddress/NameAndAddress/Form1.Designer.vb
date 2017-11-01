f5c349061b60b66b2f78182b4c37e99bac35064a
jl137275 <33095838+jl137275@users.noreply.github.com>
2017-11-01 10:35:57 -0500

    started name and address

---
+++ NameAndAddress/NameAndAddress/Form1.Designer.vb
@@ -0,0 +1,113 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.btnShowInfo = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblStreet = New System.Windows.Forms.Label()
+        Me.lblCityStateZip = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblName.Location = New System.Drawing.Point(158, 73)
+        Me.lblName.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(103, 19)
+        Me.lblName.TabIndex = 0
+        Me.lblName.Text = "William Wolfe"
+        Me.lblName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnShowInfo
+        '
+        Me.btnShowInfo.Font = New System.Drawing.Font("Times New Roman", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnShowInfo.Location = New System.Drawing.Point(76, 256)
+        Me.btnShowInfo.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnShowInfo.Name = "btnShowInfo"
+        Me.btnShowInfo.Size = New System.Drawing.Size(112, 34)
+        Me.btnShowInfo.TabIndex = 1
+        Me.btnShowInfo.Text = "Show Info"
+        Me.btnShowInfo.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Times New Roman", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(234, 256)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(112, 34)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblStreet
+        '
+        Me.lblStreet.AutoSize = True
+        Me.lblStreet.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblStreet.Location = New System.Drawing.Point(129, 107)
+        Me.lblStreet.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblStreet.Name = "lblStreet"
+        Me.lblStreet.Size = New System.Drawing.Size(160, 19)
+        Me.lblStreet.TabIndex = 3
+        Me.lblStreet.Text = "888 South Main Street"
+        Me.lblStreet.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblCityStateZip
+        '
+        Me.lblCityStateZip.AutoSize = True
+        Me.lblCityStateZip.Location = New System.Drawing.Point(124, 138)
+        Me.lblCityStateZip.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblCityStateZip.Name = "lblCityStateZip"
+        Me.lblCityStateZip.Size = New System.Drawing.Size(165, 19)
+        Me.lblCityStateZip.TabIndex = 4
+        Me.lblCityStateZip.Text = "Waynesville, NC 28786"
+        Me.lblCityStateZip.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 19.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(426, 383)
+        Me.Controls.Add(Me.lblCityStateZip)
+        Me.Controls.Add(Me.lblStreet)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnShowInfo)
+        Me.Controls.Add(Me.lblName)
+        Me.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblName As System.Windows.Forms.Label
+    Friend WithEvents btnShowInfo As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents lblStreet As System.Windows.Forms.Label
+    Friend WithEvents lblCityStateZip As System.Windows.Forms.Label
+
+End Class
---
+++ NameAndAddress/NameAndAddress/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub lblName_Click(sender As Object, e As EventArgs) Handles lblName.Click
+
+    End Sub
+
+    Private Sub btnShowInfo_Click(sender As Object, e As EventArgs) Handles btnShowInfo.Click
+
+    End Sub
+End Class
