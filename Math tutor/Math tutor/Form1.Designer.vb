b8a8a15fe3a579eea1310b87e85ae1b34f3604f0
jl137275 <33095838+jl137275@users.noreply.github.com>
2017-11-02 11:09:43 -0500

    finished math tutor

---
+++ Math tutor/Math tutor/Form1.Designer.vb
@@ -0,0 +1,76 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class lbl
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblAnswer = New System.Windows.Forms.Label()
+        Me.btnShowAnswer = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblAnswer
+        '
+        Me.lblAnswer.AutoSize = True
+        Me.lblAnswer.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAnswer.Location = New System.Drawing.Point(76, 80)
+        Me.lblAnswer.Name = "lblAnswer"
+        Me.lblAnswer.Size = New System.Drawing.Size(131, 25)
+        Me.lblAnswer.TabIndex = 0
+        Me.lblAnswer.Text = "18 + 64 = ?"
+        '
+        'btnShowAnswer
+        '
+        Me.btnShowAnswer.Location = New System.Drawing.Point(28, 172)
+        Me.btnShowAnswer.Name = "btnShowAnswer"
+        Me.btnShowAnswer.Size = New System.Drawing.Size(88, 32)
+        Me.btnShowAnswer.TabIndex = 1
+        Me.btnShowAnswer.Text = "Show Answer"
+        Me.btnShowAnswer.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(165, 172)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(83, 32)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lbl
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnShowAnswer)
+        Me.Controls.Add(Me.lblAnswer)
+        Me.Name = "lbl"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblAnswer As System.Windows.Forms.Label
+    Friend WithEvents btnShowAnswer As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
