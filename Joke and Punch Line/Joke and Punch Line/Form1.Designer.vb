ec6b015f76ec3404d2838cb232d03835eee0e9b6
jl137275 <33095838+jl137275@users.noreply.github.com>
2017-11-03 10:38:08 -0500

    finished joke and punch line

---
+++ Joke and Punch Line/Joke and Punch Line/Form1.Designer.vb
@@ -0,0 +1,76 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblJoke = New System.Windows.Forms.Label()
+        Me.btnSetup = New System.Windows.Forms.Button()
+        Me.btnPunchLine = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblJoke
+        '
+        Me.lblJoke.AutoSize = True
+        Me.lblJoke.Location = New System.Drawing.Point(23, 62)
+        Me.lblJoke.Name = "lblJoke"
+        Me.lblJoke.Size = New System.Drawing.Size(323, 13)
+        Me.lblJoke.TabIndex = 0
+        Me.lblJoke.Text = "What do you get when you combine a Christmas tree with an iPad?"
+        Me.lblJoke.Visible = False
+        '
+        'btnSetup
+        '
+        Me.btnSetup.Location = New System.Drawing.Point(26, 186)
+        Me.btnSetup.Name = "btnSetup"
+        Me.btnSetup.Size = New System.Drawing.Size(75, 23)
+        Me.btnSetup.TabIndex = 1
+        Me.btnSetup.Text = "Setup"
+        Me.btnSetup.UseVisualStyleBackColor = True
+        '
+        'btnPunchLine
+        '
+        Me.btnPunchLine.Location = New System.Drawing.Point(238, 186)
+        Me.btnPunchLine.Name = "btnPunchLine"
+        Me.btnPunchLine.Size = New System.Drawing.Size(75, 23)
+        Me.btnPunchLine.TabIndex = 2
+        Me.btnPunchLine.Text = "Punch Line"
+        Me.btnPunchLine.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(389, 262)
+        Me.Controls.Add(Me.btnPunchLine)
+        Me.Controls.Add(Me.btnSetup)
+        Me.Controls.Add(Me.lblJoke)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblJoke As System.Windows.Forms.Label
+    Friend WithEvents btnSetup As System.Windows.Forms.Button
+    Friend WithEvents btnPunchLine As System.Windows.Forms.Button
+
+End Class
---
+++ Joke and Punch Line/Joke and Punch Line/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+
+    Private Sub lblJoke_Click(sender As Object, e As EventArgs) Handles lblJoke.Click
+
+    End Sub
+
+
+    Private Sub btnSetup_Click(sender As Object, e As EventArgs) Handles btnSetup.Click
+        lblJoke.Visible = True
+
+    End Sub
+
+    Private Sub btnPunchLine_Click(sender As Object, e As EventArgs) Handles btnPunchLine.Click
+        lblJoke.Text = ("A pineapple")
+    End Sub
+End Class
